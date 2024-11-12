b2fb2e0eff2fc34321b1bcf1627f474eb433ceef
Trung <gptvj54@gmail.com>
2024-11-12 06:22:37 +0000

    update: MyReadme.md

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set CUDA device to 1
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 
 # Disable NCCL P2P and IB for compatibility with RTX 4000 series
 export NCCL_P2P_DISABLE=1
@@ -12,13 +12,13 @@ python3 run.py \
         --model_name "google/mt5-large" \
         --run_name "docTquery-MSMARCO" \
         --max_length 128 \
-        --train_file data/msmarco_data/100k/msmarco_docTquery_train_data.json \
-        --valid_file data/msmarco_data/100k/msmarco_docTquery_dev_data.json \
+        --train_file data/msmarco_data/100k/msmarco_DSI_train_data.json \
+        --valid_file data/msmarco_data/100k/msmarco_DSI_dev_data.json \
         --output_dir "models/msmarco_docTquery_mt5_large" \
         --learning_rate 0.0001 \
         --warmup_steps 0 \
-        --per_device_train_batch_size 4 \
-        --per_device_eval_batch_size 4 \
+        --per_device_train_batch_size 2 \
+        --per_device_eval_batch_size 2 \
         --evaluation_strategy steps \
         --eval_steps 100 \
         --max_steps 2000 \
@@ -27,7 +27,7 @@ python3 run.py \
         --save_steps 100 \
         --save_total_limit 2 \
         --load_best_model_at_end \
-        --gradient_accumulation_steps 4 \
+        --gradient_accumulation_steps 2 \
         --report_to wandb \
         --logging_steps 100 \
         --dataloader_drop_last False
