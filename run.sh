12d65e178c49afeea8e5cec1deb7e08f7f68fe88
Trung <gptvj54@gmail.com>
2024-11-13 06:57:57 +0000

    update run.sh

--- run.sh
+++ run.sh
@@ -7,27 +7,30 @@ export CUDA_VISIBLE_DEVICES=0
 export NCCL_P2P_DISABLE=1
 export NCCL_IB_DISABLE=1
 
+## Step 3: Train DSI-QG with query-represented corpus
 python3 run.py \
-        --task "docTquery" \
-        --model_name "google/mt5-large" \
-        --run_name "docTquery-MSMARCO" \
+        --task "DSI" \
+        --model_name "google/mt5-base" \
+        --run_name "MSMARCO-20k-mt5-base-DSI" \
         --max_length 128 \
-        --train_file data/msmarco_data/100k/msmarco_DSI_train_data.json \
-        --valid_file data/msmarco_data/100k/msmarco_DSI_dev_data.json \
-        --output_dir "models/msmarco_docTquery_mt5_large" \
-        --learning_rate 0.0001 \
-        --warmup_steps 0 \
-        --per_device_train_batch_size 2 \
-        --per_device_eval_batch_size 2 \
+        --train_file data/msmarco_data/20k/train_retrieval_ms_marco.json \
+        --valid_file data/msmarco_data/20k/validation_retrieval_ms_marco.json \
+        --output_dir "models/MSMARCO-20k-mt5-base-DSI" \
+        --learning_rate 0.0005 \
+        --warmup_steps 10000 \
+        --per_device_train_batch_size 4 \
+        --per_device_eval_batch_size 4 \
         --evaluation_strategy steps \
-        --eval_steps 100 \
-        --max_steps 2000 \
+        --eval_steps 1000 \
+        --max_steps 100000 \
         --save_strategy steps \
-        --dataloader_num_workers 10 \
-        --save_steps 100 \
+        --dataloader_num_workers 2 \
+        --save_steps 1000 \
         --save_total_limit 2 \
         --load_best_model_at_end \
         --gradient_accumulation_steps 2 \
         --report_to wandb \
         --logging_steps 100 \
-        --dataloader_drop_last False
+        --dataloader_drop_last False \
+        --metric_for_best_model Recall@50 \
+        --greater_is_better True
